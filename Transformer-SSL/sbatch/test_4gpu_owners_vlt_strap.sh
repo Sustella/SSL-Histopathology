375c2d085b877838560e8c6313e8a643d09182f0
Rikiya Yamashita <rikiya@sh03-ln07.stanford.edu>
2021-08-02 07:29:41 -0700

    update build.py and added sbatch scripts

--- Transformer-SSL/sbatch/test_4gpu_owners_vlt_strap.sh
+++ Transformer-SSL/sbatch/test_4gpu_owners_vlt_strap.sh
@@ -4,10 +4,10 @@
 #SBATCH --error=test_4gpu_owners_vlt_strap.err
 #SBATCH --time=2-00:00:00
 #SBATCH -p owners
-#SBATCH --gpus 4
+#SBATCH --gpus 2
 #SBATCH --gpu_cmode=shared 
 #SBATCH -C GPU_MEM:32GB
-#SBATCH --mem 128G
+#SBATCH --mem 64G
 #SBATCH --nodes 1
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=rikiya@stanford.edu
@@ -18,6 +18,5 @@ conda activate transformer-ssl
 module load cuda/10.2.89
 
 nvidia-smi
-cd ~/contrastive_strap/Transformer-SSL/
-# python -m torch.distributed.launch --nproc_per_node 4 --master_port 12345  moby_main.py --cfg configs/moby_swin_tiny.yaml --data-path /scratch/groups/rubin/rikiya/ssl_pretrain_tiny --batch-size 128 --opts TRAIN.EPOCHS 300 --output /scratch/users/rikiya/ssl_pretrain_test_owners_4vlt
-python -m torch.distributed.launch --nproc_per_node 4 --master_port 12345  moby_main.py --cfg configs/moby_swin_tiny_256.yaml --data-path /scratch/groups/rubin/rikiya/ssl_pretrain_tiny --batch-size 128 --opts TRAIN.EPOCHS 300 DATA.DATASET 'wsi' --output /scratch/users/stellasu/ssl_swin
+cd ~/contrastive_strap/SSL-Transformer-Histopathology/Transformer-SSL/
+python -m torch.distributed.launch --nproc_per_node 2 --master_port 12345  moby_main.py --cfg configs/moby_swin_tiny.yaml --data-path /scratch/groups/rubin/rikiya/sample --batch-size 128 --opts TRAIN.EPOCHS 5 DATA.DATASET 'wsi' AUG.TRANSFORMATION 'strap' --output /scratch/users/rikiya/ssl_pretrain_test_owners_4vlt_strap --amp-opt-level O2
